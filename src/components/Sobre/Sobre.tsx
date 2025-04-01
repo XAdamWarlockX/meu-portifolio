f5ba7be78ce8e44f4d74b3040e3b2e20de47c4f6
Adam Warlock <adam.warlock.pf@gmail.com>
2025-04-01 10:11:09 -0300

    Mudanças no sobre

--- src/components/Sobre/Sobre.tsx
+++ src/components/Sobre/Sobre.tsx
@@ -8,11 +8,11 @@ export const Sobre = () => {
                 <H1>Adam Warlock</H1>
                 <H3>Desenvolvedor Web Front-end</H3>
 
-                <P><strong>Olá! Me chamo Adam Warlock e sou um desenvolvedor web frontend em formação!</strong> Gosto MUITO da área de tecnologia, e tenho certeza que posso contribuir em qualquer time com as minhas habilidades.</P>
+                <P><strong>Olá! Me chamo Adam Warlock e sou um desenvolvedor web frontend em formação!</strong> Gosto muito da área de tecnologia, e tenho certeza que posso contribuir em qualquer time com as minhas habilidades.</P>
 
-                <P>Estou sempre me desafiando com novos projetos e participando de comunidades de programação para buscar feedback de outros desenvolvedores e desenvolvedoras. Além de tentar ajudar essas pessoas com o que eu já aprendi. 😎</P>
+                <P>Estou sempre me desafiando com novos projetos e participando de comunidades de programação.  Além de tentar ajudar essas pessoas com o que eu já aprendi. 😎</P>
 
-                <P>Ah, gosto de jogos, filmes, séries, animes e outras nerdices. 💙</P>
+                <P>Ah, também gosto de jogos, filmes, séries, animes e de treinar para dar uma equilibrada, esse é o meu universo. 🤓</P>
             </InfosSobre>
 
             <Img src={fotoPerfil} alt="foto do perfil do Ricardo" />
